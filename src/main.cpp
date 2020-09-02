aae71e8e83491265d15128dd44b647680b3ac91e
ullmans <shilo.ullman@gmail.com>
2020-09-02 20:48:30 +0300

    added a main function

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,98 @@
 #include <string>
 #include <stdint.h>
+#include <iostream>
+#include <fstream>
 
 #include "Matrix.hpp"
 #include "CacheManager.hpp"
 #include "bmp_tester.hpp"
+#include "matrix_tester.hpp"
 #include "BMP.hpp"
+#include "file_reading.hpp"
+#include "MessageException.hpp"
 
-int main(uint32_t argc, char* argv[]){
-    if(argv[1] == "matrix"){
-        if(argv[2] == "add"){
+int main(int argc, char* argv[]){
+    CacheManager cache = CacheManager(100);   //what is the size?
 
-        } else if(argv[2] == "multiply"){
+    //for matrix
+    if(std::string(argv[1]).compare("matrix") != 0){
+        matrixArguments(cache, argv);
 
-        } else {
-            //throw exception
-        }
+    //for image (BMP)
+    } else if(std::string(argv[1]).compare("image") != 0){
+        imageArguments(cache, argv);
+    
+    //for hash
+    } else if(std::string(argv[1]).compare("hash") != 0){
+        hashArguments(cache, argv);
 
-    } else if(argv[1] == "image"){
-        if(argv[2] == "rotate"){
+    //for cache
+    } else if(std::string(argv[1]).compare("cache") != 0){
+        cacheArguments(cache, argv);
 
-        } else if(argv[2] == "convert"){
-
-        } else {
-            //throw exception
-        }
-
-    } else if(argv[1] == "hash"){
-        if(argv[2] == "algorithem"){
-        
-        } else {
-            //throw exception
-        }
-
-    } else if(argv[1] == "cache"){
-        if(argv[2] == "clear"){
-            //need to add a clear function to cacheManager
-        } else if(argv[2] == "search"){
-            
-        }
-
+    //not a valid command
     } else {
-        //throw exception
+        throw MessageException("the argunments are not a valid command");
+    }
+}
+
+void matrixArguments(CacheManager cache, char* argv[]){
+    //check if we have the action in the cache
+    std:: string path = "MATRIX_" + std::string(argv[2]) + '_' + std::string(argv[3]) + '_' + std::string(argv[4]);
+    //1 -> if we have it
+    if(cache.search(path)){
+        cache.insert(path, cache.get(path));
+    //0 -> if we do not have it
+    } else {
+        if(std::string(argv[2]).compare("add") != 0){
+            //add the matrices
+            testing::matrix::add(cache,path, argv);
+        } else if(std::string(argv[2]).compare("multiply") != 0){
+            //multiply the matrices
+            testing::matrix::multiply(cache,path, argv);
+        } else {
+            throw MessageException("the argunments are not a valid command");
+        }
+    }
+}
+
+void imageArguments(CacheManager cache, char* argv[]){
+    //check if we have the action in the cache
+    std:: string path = "IMAGE_" + std::string(argv[3]) + '_' + std::string(argv[2]) + '_' + std::string(argv[4]);
+    //1 -> if we have it
+    if(cache.search(path)){
+        cache.insert(path, cache.get(path));
+    //0 -> if we dont have it
+    } else {
+        if(std::string(argv[2]).compare("rotate") != 0){
+            //rotate the image
+            testing::bmp::rotate_image(cache,path, argv);
+        } else if(std::string(argv[2]).compare("convert") != 0){
+            //convert the image
+            testing::bmp::convert_to_grayscale(cache,path, argv);
+        } else {
+            throw MessageException("the argunments are not a valid command");
+        }
+    }
+}
+
+void hashArguments(CacheManager cache, char* argv[]){
+    if(std::string(argv[2]).compare("algorithem") != 0){
+    //calculate hash
+    } else {
+        throw MessageException("the argunments are not a valid command");
+    }
+}
+
+void cacheArguments(CacheManager cache, char* argv[]){
+    if(std::string(argv[2]).compare("clear") != 0){
+        cache.clear();
+    } else if(std::string(argv[2]).compare("search") != 0){
+        std:: string path = "CACHE_" + std::string(argv[3]) + '_' + std::string(argv[2]) + '_' + std::string(argv[4]);
+        if(cache.searh(path)){
+            printf("result found in cache");
+        }
+    } else {
+        throw MessageException("the argunments are not a valid command");
     }
 }
