92bfd687123251083921c51e0e28d79198dc1c2b
ullmans <shilo.ullman@gmail.com>
2020-09-03 11:00:23 +0300

    build the program and run it first time

---
+++ src/CMakeFiles/ex3.out.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ullmans/ex03/ex03/src/BMP.cpp" "/home/ullmans/ex03/ex03/src/CMakeFiles/ex3.out.dir/BMP.cpp.o"
+  "/home/ullmans/ex03/ex03/src/CacheManager.cpp" "/home/ullmans/ex03/ex03/src/CMakeFiles/ex3.out.dir/CacheManager.cpp.o"
+  "/home/ullmans/ex03/ex03/src/Matrix.cpp" "/home/ullmans/ex03/ex03/src/CMakeFiles/ex3.out.dir/Matrix.cpp.o"
+  "/home/ullmans/ex03/ex03/src/MessageException.cpp" "/home/ullmans/ex03/ex03/src/CMakeFiles/ex3.out.dir/MessageException.cpp.o"
+  "/home/ullmans/ex03/ex03/src/bmp_tester.cpp" "/home/ullmans/ex03/ex03/src/CMakeFiles/ex3.out.dir/bmp_tester.cpp.o"
+  "/home/ullmans/ex03/ex03/src/file_reading.cpp" "/home/ullmans/ex03/ex03/src/CMakeFiles/ex3.out.dir/file_reading.cpp.o"
+  "/home/ullmans/ex03/ex03/src/main.cpp" "/home/ullmans/ex03/ex03/src/CMakeFiles/ex3.out.dir/main.cpp.o"
+  "/home/ullmans/ex03/ex03/src/matrix_tester.cpp" "/home/ullmans/ex03/ex03/src/CMakeFiles/ex3.out.dir/matrix_tester.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- src/matrix_tester.cpp
+++ src/matrix_tester.cpp
@@ -6,15 +6,15 @@
 #include "CacheManager.hpp"
 
 void testing::matrix::add(CacheManager cache, const std::string& key, char* argv[]){
-    Matrix matrix1(readTextFileContent(std::string(argv[3])));
-    Matrix matrix2(readTextFileContent(std::string(argv[4])));
+    Matrix matrix1(readFileContent(std::string(argv[3])));
+    Matrix matrix2(readFileContent(std::string(argv[4])));
     matrix1 += matrix2;
     cache.insert(key, std::string(argv[5]), matrix1.toString());
 }
 
 void testing::matrix::multiply(CacheManager cache, const std::string& key, char* argv[]){
-    Matrix matrix1(readTextFileContent(std::string(argv[3])));
-    Matrix matrix2(readTextFileContent(std::string(argv[4])));
+    Matrix matrix1(readFileContent(std::string(argv[3])));
+    Matrix matrix2(readFileContent(std::string(argv[4])));
     matrix1 *= matrix2;
     cache.insert(key, std::string(argv[5]), matrix1.toString());
 }
